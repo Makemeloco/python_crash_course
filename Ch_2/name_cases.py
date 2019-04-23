7e4bb17b4c61c10a20963982c1d275b4d358b196
Kate <make.me.loco@gmail.com>
2019-04-23 18:33:07 +0300

    Strings. (p.42)

---
+++ Ch_2/name_cases.py
@@ -0,0 +1,7 @@
+name = 'Kate'
+massage = 'Hello ' + name.title() + ' ' + name.upper() + ' ' + name.lower() + ', would you like to learn some Python today?'
+print(massage)
+
+famous_person = '\t\t\nкитайцы\t\n\n'
+massage_2 = 'Наблюдательные ' + famous_person.lstrip()+ ' ' + famous_person.rstrip() + ' ' + famous_person.strip() + ' говорили: "вся вселенная помещается в чайнике".'
+print(massage_2)
