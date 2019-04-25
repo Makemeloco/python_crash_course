da4c1a1d9793c561cf6ccbd79645f6f0b9fc8eb2
Kate <make.me.loco@gmail.com>
2019-04-25 19:02:34 +0300

    Habr. (p.53)

--- Ch_3/motorcycles.py
+++ Ch_3/motorcycles.py
@@ -1,6 +1,26 @@
+#! python3
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 motorcycles[0] = 'ducati'
 print(motorcycles)
 motorcycles.append('ducati')
 print(motorcycles)
+
+
+a = 1
+b = 2
+print(a)
+print(b)
+a, b = b, a
+print(a)
+print(b)
+
+l = ['David', 'Pythonista', '+1-514-555-1234']
+people = [l, ['Guido', 'BDFL', 'unlisted']]
+
+for (name, title, phone) in people:
+    print(name, phone)
+
+
+colors = ['red', 'blue', 'green', 'yellow']
+print('Choose ' + ', '.join(colors[:-1]) + ' or ' + colors[-1] + '.')
