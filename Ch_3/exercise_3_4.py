433d30ba094db896df96a73dc490a5547d431de8
Unknown <make.me.loco@gmail.com>
2019-04-28 23:16:00 +0300

    Exercise 3.5-3.6 (p.56)

--- Ch_3/exercise_3_4.py
+++ Ch_3/exercise_3_4.py
@@ -1,4 +1,21 @@
 guests = ['Tonya', 'Alisa', 'Samara', 'Ella', 'Stefany']
 
-for i in guests:
-    print('Hello, ' + i + '! Come to my place for lunch on Wednesday at 14 o\'clock.')
+def invitations():
+    for i in guests:
+        print('Hello, ' + i + '! Come to my place for lunch on Wednesday at 14 o\'clock.')
+    print('\n')
+
+invitations()
+
+print(guests[0] + ' will not be able to come.')
+
+guests[0] = 'Aleksa'
+
+invitations()
+
+print('There will be more guests.')
+
+guests.insert(0, 'Amy')
+guests.insert(3, 'Rihanna')
+guests.append('Ann')
+invitations()
