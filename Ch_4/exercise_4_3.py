18b660bf7f5398f55041e39c0f7322e072a96c52
Unknown <make.me.loco@gmail.com>
2019-05-04 00:13:02 +0300

    p.72

---
+++ Ch_4/exercise_4_3.py
@@ -0,0 +1,17 @@
+nums = []
+nums = [value + 1 for value in range(0, 20)]
+print(nums)
+digits = []
+digits = [value + 1 for value in range(0, 1000000)]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+nums2 = []
+nums2 = [value + 1 for value in range(0, 20, 2)]
+print(nums2)
+squares = []
+squares = [value + 3 for value in range(0, 30, 3)]
+print(squares)
+quatros = []
+quatros = [value**3 for value in range(1,11)]
+print(quatros)
