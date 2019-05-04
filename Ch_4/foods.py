f8089b18328c5e0ffe3a7706d1b2d37088bf1188
Unknown <make.me.loco@gmail.com>
2019-05-04 15:50:02 +0300

    p.75 a

---
+++ Ch_4/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:" + str(my_foods))
+print(my_foods)
+
+print("\nMy friend's favorite foods are:" + str(friend_foods))
+print(friend_foods)
