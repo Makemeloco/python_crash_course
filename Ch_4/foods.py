34a0e073350d1e05effebaf21cb7a0196d39e5f9
Unknown <make.me.loco@gmail.com>
2019-05-05 23:34:40 +0300

    p.77

--- Ch_4/foods.py
+++ Ch_4/foods.py
@@ -4,5 +4,14 @@ friend_foods = my_foods[:]
 my_foods.append('cannoli')
 friend_foods.append('ice cream')
 
-print("My favorite foods are:" + str(my_foods))
-print("\nMy friend's favorite foods are:" + str(friend_foods))
+print("My favorite foods are:")
+for food in my_foods:
+    print(food.title())
+print("\n")
+
+print("\nMy friend's favorite foods are:")
+for friend_food in friend_foods:
+    print(friend_food.title())
+
+print('\nThe last three items in the list are:' + str(my_foods[-3:] ))
+print('\nThe first three items in the list are:' + str(my_foods[:3] ))
--- Ch_4/pizza_hot.py
+++ Ch_4/pizza_hot.py
@@ -1,4 +1,17 @@
 pizzas = ['Гавайская', 'Пепперони', '4 сыра']
 for pizza in pizzas:
     print(pizza.title() + " - пицца, которую я люблю!")
-print("Я действительно люблю пиццу!")
+print("Я действительно люблю пиццу!\n")
+
+friend_pizzas = pizzas[:]
+pizzas.append('Маргарита')
+friend_pizzas.append('Мясная ассорти')
+
+
+for pizza in pizzas:
+    print(pizza.title() + " - пицца, которую я люблю!")
+print("Я действительно люблю пиццу!\n")
+
+for friend_pizza in friend_pizzas:
+    print(friend_pizza.title() + " - пицца, которую любит моя подруга!")
+print("Она действительно любит пиццу!")
