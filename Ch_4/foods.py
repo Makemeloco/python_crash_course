24dce5324e20fa8fb1c2cd807763c12d71b3f388
Unknown <make.me.loco@gmail.com>
2019-05-05 17:17:09 +0300

    p.76

--- Ch_4/foods.py
+++ Ch_4/foods.py
@@ -5,7 +5,4 @@ my_foods.append('cannoli')
 friend_foods.append('ice cream')
 
 print("My favorite foods are:" + str(my_foods))
-print(my_foods)
-
 print("\nMy friend's favorite foods are:" + str(friend_foods))
-print(friend_foods)
