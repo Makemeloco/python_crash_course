98f963391c567a60aa2415613bf4a6219d2cf49b
Kate <make.me.loco@gmail.com>
2019-05-06 18:18:27 +0300

    p.81

---
+++ Ch_4/buffet.py
@@ -0,0 +1,8 @@
+buffet = ('apple pie', 'pizza', 'soup', 'sandwiches', 'pancakes')
+for food in buffet:
+    print(food)
+print('\n')
+buffet = ('taco', 'spagetti', 'soup', 'sandwiches', 'pancakes')
+for food in buffet:
+    print(food)
+    
