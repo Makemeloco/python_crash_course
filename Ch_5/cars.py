c02798f112c6cec96243d7aa4d10f7f4d9110f1f
Kate <make.me.loco@gmail.com>
2019-05-07 13:35:17 +0300

    p.89

--- Ch_5/cars.py
+++ Ch_5/cars.py
@@ -4,3 +4,9 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
