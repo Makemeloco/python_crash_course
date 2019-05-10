13f87e0dc7f4c16e1beb6035ebfe395543a68706
Unknown <make.me.loco@gmail.com>
2019-05-10 21:42:37 +0300

    p.95 a

---
+++ Ch_5/alien_color_2.py
@@ -0,0 +1,8 @@
+alien_color = 'red'
+score = 0
+if alien_color == 'green':
+    score += 5
+    print('Your score is ' + str(score) + ' coins.')
+else:
+    score += 10
+    print('Your score is ' + str(score) + ' coins.')
