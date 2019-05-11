e984dd0796b177784762f00ff7f84371019270c5
Unknown <make.me.loco@gmail.com>
2019-05-11 23:22:55 +0300

    p.96

---
+++ Ch_5/fruits.py
@@ -0,0 +1,9 @@
+favorite_fruits = ['banana', 'grape', 'mango', 'pineapple', 'orange', 'pamelo']
+
+if 'mango' in favorite_fruits:
+    print('You really like mango!')
+if 'pamelo' in favorite_fruits:
+    print('You really like pamelo!')
+if 'banana' in favorite_fruits:
+    print('You really like banana!')
+    
--- Ch_5/toppings.py
+++ Ch_5/toppings.py
@@ -1,13 +1,7 @@
-requested_topping = 'mushrooms'
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
 
-requested_toppings = ['mushrooms', 'extra cheese']
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
 
 print("\nFinished making your pizza!")
