29a2c04a06d0e91cc5590f1feab3addaa55abfe1
Unknown <make.me.loco@gmail.com>
2019-05-11 23:46:37 +0300

    p.99

---
+++ Ch_5/hello_admin.py
@@ -0,0 +1,7 @@
+usernames = ['Katie', 'Jane', 'admin', 'Dominic', 'Anna', 'Lisa', 'Mary']
+
+for username in usernames:
+    if username == 'admin':
+        print('Hello admin, would you like to see a status report?')
+    else:
+        print('Hello ' + username + ', thank you for logging in again!')
