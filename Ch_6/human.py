500c76e787ada2ef4c92a8f40f6e0bc5ede2c983
Unknown <make.me.loco@gmail.com>
2019-05-12 22:59:09 +0300

    p.108

---
+++ Ch_6/human.py
@@ -0,0 +1,6 @@
+human = {'first_name': 'Katie', 'last_name': 'Abadzheva', 'age': 30, 'city': 'Saint-Petersburg'}
+
+print(human)
+for key, value in human.items():
+    print("\nKey: " + key)
+    print("Value: " + str(value))
