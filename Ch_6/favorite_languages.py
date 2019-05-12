b5b0217294f9bac1b3d00d9334e48df3b1ee9faa
Unknown <make.me.loco@gmail.com>
2019-05-12 23:09:11 +0300

    p.111

--- Ch_6/favorite_languages.py
+++ Ch_6/favorite_languages.py
@@ -4,5 +4,21 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python',
 }
-print("Sarah's favorite language is " +
-      favorite_languages['sarah'].title() + ".")
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+for lang in favorite_languages.values():
+    print(lang.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() + ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
