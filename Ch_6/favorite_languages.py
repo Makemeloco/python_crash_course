fb152735328df410699f5ef96a213d9fd0880a72
Unknown <make.me.loco@gmail.com>
2019-05-13 23:21:12 +0300

    p.112

--- Ch_6/favorite_languages.py
+++ Ch_6/favorite_languages.py
@@ -26,3 +26,14 @@ for name in favorite_languages.keys():
 
 if 'erin' not in favorite_languages.keys():
     print("Erin, please take our poll!")
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
