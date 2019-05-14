fe6fcb689c18ca7eb38a34cf815e1f1733a73d67
Kate <make.me.loco@gmail.com>
2019-05-14 17:45:11 +0300

    p.117 a

--- Ch_6/favorite_languages_2.py
+++ Ch_6/favorite_languages_2.py
@@ -1,11 +1,15 @@
 favorite_languages = {
-'jen': ['python', 'ruby'],
-'sarah': ['c'],
-'edward': ['ruby', 'go'],
-'phil': ['python', 'haskell'],
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
 }
 
 for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite languages are:")
-    for language in languages:
-        print("\t" + language.title())
+    if len(languages) < 2:
+        print("\n" + name.title() + "'s favorite languages is " +
+              languages[0].title() + '.')
+    else:
+        print("\n" + name.title() + "'s favorite languages are:")
+        for language in languages:
+            print("\t" + language.title())
