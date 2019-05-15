7683f39a1c7eff66e5ecab64d2da58c385fb8058
Unknown <make.me.loco@gmail.com>
2019-05-16 00:32:41 +0300

    p.125

--- Ch_7/parrot.py
+++ Ch_7/parrot.py
@@ -10,3 +10,13 @@ prompt = "If you tell us who you are, we can personalize the messages you see."
 prompt += "\nWhat is your first name? "
 name = input(prompt)
 print("\nHello, " + name + "!")
+
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+    
