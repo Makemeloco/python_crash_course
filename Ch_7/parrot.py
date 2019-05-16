d3f3057d5d6b713a5d364469102f873f58f9fd6b
Unknown <make.me.loco@gmail.com>
2019-05-16 23:56:48 +0300

    p.127

--- Ch_7/parrot.py
+++ Ch_7/parrot.py
@@ -1,22 +1,13 @@
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
-
-
-name = input("Please enter your name: ")
-print("Hello, " + name + "!")
-
-
-prompt = "If you tell us who you are, we can personalize the messages you see."
-prompt += "\nWhat is your first name? "
-name = input(prompt)
-print("\nHello, " + name + "!")
-
 
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 message = ""
 
-while message != 'quit':
+active = True
+while active:
     message = input(prompt)
-    if message != 'quit':
-        print(message)
+
+    if message == 'quit':
+            active = False
+    else:
+            print(message)
