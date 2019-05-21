57cf8d386471246fca906afee7f3e0f43a2a50c1
Unknown <make.me.loco@gmail.com>
2019-05-21 23:32:23 +0300

    p.137

--- Ch_8/greeter.py
+++ Ch_8/greeter.py
@@ -1,5 +1,6 @@
-def greet_user():
-    """Выводит простое приветствие."""
-    print("Hello!")
 
-greet_user()
+def greet_user(username):
+    """Выводит простое приветствие."""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
