183bf963779cdc222e01d9b00fee5ebc1b26475e
Unknown <make.me.loco@gmail.com>
2019-05-21 23:54:41 +0300

    p.142

---
+++ Ch_8/make_shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(shirt_size, shirt_text):
+    """Выводит информацию о футболке."""
+    print("\nРазмер футболки: " + shirt_size + ".")
+    print("Текст на футболке: " + shirt_text + ".")
+
+make_shirt('L', 'Возвращаемое значение')
+make_shirt(shirt_size='L', shirt_text='Возвращаемое значение')
