fe3ce779efb9c1fa80b1534032b3b4d1c96ff71d
Unknown <make.me.loco@gmail.com>
2019-05-21 23:48:50 +0300

    p.141

--- Ch_8/pets.py
+++ Ch_8/pets.py
@@ -1,4 +1,4 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='dog'):
     """Выводит информацию о животном."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
@@ -7,3 +7,5 @@ describe_pet('hamster', 'harry')
 describe_pet('dog', 'willie')
 describe_pet(animal_type='hamster', pet_name='harry')
 describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(pet_name='willie')
+describe_pet('willie')
