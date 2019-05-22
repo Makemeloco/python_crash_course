95400913004c7588f6add1fb0809bc583e85de49
Kate <make.me.loco@gmail.com>
2019-05-22 18:52:40 +0300

    p.142 a

--- Ch_8/make_shirt.py
+++ Ch_8/make_shirt.py
@@ -1,7 +1,8 @@
-def make_shirt(shirt_size, shirt_text):
+def make_shirt(shirt_size='L', shirt_text='I love Python.'):
     """Выводит информацию о футболке."""
     print("\nРазмер футболки: " + shirt_size + ".")
     print("Текст на футболке: " + shirt_text + ".")
 
 make_shirt('L', 'Возвращаемое значение')
-make_shirt(shirt_size='L', shirt_text='Возвращаемое значение')
+make_shirt(shirt_size='M', shirt_text='Возвращаемое значение')
+make_shirt()
