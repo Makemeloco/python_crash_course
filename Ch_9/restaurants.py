eaf56fa1d1b18079ffdb63559c3b69af2c5b2fd5
Kate <make.me.loco@gmail.com>
2019-06-04 19:00:25 +0300

    p.165

---
+++ Ch_9/restaurants.py
@@ -0,0 +1,6 @@
+class Restaurant():
+    """docstring for Restaurant."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super(Restaurant, self).__init__()
+        self.restaurant_name = restaurant_name
