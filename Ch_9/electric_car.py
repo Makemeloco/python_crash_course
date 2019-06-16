3e5904cad58240938d886a53314c8dad4cdb2276
Unknown <make.me.loco@gmail.com>
2019-06-16 22:50:23 +0300

    p.175

--- Ch_9/electric_car.py
+++ Ch_9/electric_car.py
@@ -27,13 +27,24 @@ class Car():
 class Battery():
     """Простая модель аккумулятора электромобиля."""
 
-    def __init__(self, battery_size=170):
+    def __init__(self, battery_size=70):
         """Инициализирует атрибуты аккумулятора."""
         self.battery_size = battery_size
 
     def describe_battery(self):
         """Выводит информацию о мощности аккумулятора."""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Выводит приблизительный запас хода для аккумулятора."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
 
 class ElectricCar(Car):
@@ -55,3 +66,4 @@ my_tesla = ElectricCar('tesla', 'model s', 2016)
 
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
