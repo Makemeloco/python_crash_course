b2ca83e83028fd7f6c5384d140682d58643df78c
Kate <make.me.loco@gmail.com>
2019-06-18 17:13:43 +0300

    p.180

--- Ch_9/my_car.py
+++ Ch_9/my_car.py
@@ -1,7 +1,14 @@
-from import_car_class import Car
+import car_class
 
-my_new_car = Car('audi', 'a4', 2016)
+my_new_car = car_class.Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+
+my_beetle = car_class.Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = car_class.ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
