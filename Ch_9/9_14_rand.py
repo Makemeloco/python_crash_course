b1cad580407d2066b3bf1e368c36d53685675ee0
Kate <make.me.loco@gmail.com>
2019-06-19 17:20:59 +0300

    9_14

---
+++ Ch_9/9_14_rand.py
@@ -0,0 +1,25 @@
+from random import randint
+
+class Die:
+    """класс, описывающий кубик."""
+
+    def __init__(self, sides=6):
+        """Инициализируем атрибуты кубика."""
+
+        self.sides = sides
+
+    def roll_die(self, rolls):
+        """бросок кубика"""
+        for i in range(0, rolls):
+            x = randint(1, self.sides)
+            print(x)
+
+
+six_sides_die = Die()
+six_sides_die.roll_die(10)
+print('\n\n\n')
+ten_sides_die = Die(10)
+ten_sides_die.roll_die(10)
+print('\n\n\n')
+twenty_sides_die = Die(20)
+twenty_sides_die.roll_die(10)
