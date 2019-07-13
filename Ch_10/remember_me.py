d7987862da19ff41597ea9916ad051a45393be2c
Unknown <make.me.loco@gmail.com>
2019-07-13 14:35:15 +0300

    p.209

--- Ch_10/remember_me.py
+++ Ch_10/remember_me.py
@@ -1,19 +1,36 @@
 import json
 
 
-def greet_user():
-    """Приветствует пользователя по имени."""
+def get_stored_username():
+    """Получает хранимое имя пользователя, если оно существует."""
     filename = 'username.json'
     try:
         with open(filename) as f_obj:
             username = json.load(f_obj)
     except FileNotFoundError:
-        username = input("What is your name? ")
-        with open(filename, 'w') as f_obj:
-            json.dump(username, f_obj)
-            print("We'll remember you when you come back, " + username + "!")
+        return None
     else:
-        print("Welcome back, " + username + "!")
+        return username
+
+
+def get_new_username():
+    """Запрашивает новое имя пользователя"""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!")
+
+
+def greet_user():
+    """Приветствует пользователя по имени."""
+    username = get_stored_username()
+    if username:
+        answer = input("Hello! You are " + username + "? y/n\n")
+        if answer == 'y':
+            print("Welcome back, " + username + "!")
+        else:
+            get_new_username()
 
 
 greet_user()
