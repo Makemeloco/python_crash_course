088c25b1d432baf6e75d3e3fc2f67c823b3fdc45
Unknown <make.me.loco@gmail.com>
2019-07-13 12:41:35 +0300

    p.207

--- Ch_10/remember_me.py
+++ Ch_10/remember_me.py
@@ -1,13 +1,19 @@
 import json
 
-filename = 'username.json'
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except FileNotFoundError:
-    username = input("What is your name? ")
-    with open(filename, 'w') as f_obj:
-        json.dump(username, f_obj)
-        print("We'll remember you when you come back, " + username + "!")
-else:
-    print("Welcome back, " + username + "!")
+
+def greet_user():
+    """Приветствует пользователя по имени."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + username + "!")
+    else:
+        print("Welcome back, " + username + "!")
+
+
+greet_user()
