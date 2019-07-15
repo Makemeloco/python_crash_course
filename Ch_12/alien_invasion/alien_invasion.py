9a147579dc1aec1ab1730171575a5709e2b9b751
Unknown <make.me.loco@gmail.com>
2019-07-16 00:33:55 +0300

    p.234

--- Ch_12/alien_invasion/alien_invasion.py
+++ Ch_12/alien_invasion/alien_invasion.py
@@ -7,6 +7,9 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    # Назначаем цвет фона.
+    bg_color = (0, 25, 51)
+
 
     # Запуск основного цикла игры.
     while True:
@@ -14,6 +17,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # При каждом проходе цикла прорисовывается экран.
+        screen.fill(bg_color)
+
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
