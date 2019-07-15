67e3c57adf866fea9793be479c1c821e8809229b
Unknown <make.me.loco@gmail.com>
2019-07-15 23:13:45 +0300

    p.231

---
+++ Ch_12/alien_invasion/alien_invasion.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+def run_game():
+    # Инициализирует игру и создает объект экрана.
+    pygame.init()
+    screen = pygame.set_caption("Alien Invasion")
+
+    # Запуск основного цикла игры.
+    
