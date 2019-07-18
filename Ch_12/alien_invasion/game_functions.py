fc93c05637e1f46a06e1314a353da7ca0b3a4105
Unknown <make.me.loco@gmail.com>
2019-07-19 01:34:09 +0300

    p.255

--- Ch_12/alien_invasion/game_functions.py
+++ Ch_12/alien_invasion/game_functions.py
@@ -2,15 +2,16 @@ import sys
 import pygame
 from bullet import Bullet
 
+
 def update_bullets(bullets):
     """Обновляет позиции пуль и уничтожает старые пули."""
     # Обновление позиций пуль.
     bullets.update()
-
     # Удаление пуль, вышедших за край экрана.
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагируут на нажатие клавиш."""
@@ -19,10 +20,15 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Создание новой пули и включение ее в группу bullets.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум ещё не достигнут."""
+    # Создание новой пули и включение ее в группу bullets.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
