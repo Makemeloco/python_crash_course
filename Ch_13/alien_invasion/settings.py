e254ae3a837b43a93f2dfab773e20454fcb3d0dc
Unknown <make.me.loco@gmail.com>
2019-07-19 11:25:26 +0300

    p.257

---
+++ Ch_13/alien_invasion/settings.py
@@ -0,0 +1,20 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 25, 51)
+
+        # Настройки корабля.
+        self.ship_speed_factor = 1.5
+
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 255, 255, 0
+        self.bullets_allowed = 3
+
