ef10a86d94375e44bbb835eaceb45e40d6eebb1c
Unknown <make.me.loco@gmail.com>
2019-07-21 22:31:20 +0300

    p.269

--- Ch_13/alien_invasion/game_functions.py
+++ Ch_13/alien_invasion/game_functions.py
@@ -101,8 +101,27 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_aliens(aliens):
-    """Обновляет позиции всех пришельцев во флоте."""
+def check_fleet_edges(ai_settings, aliens):
+    """Реагирует на достижение пришельцем края экрана."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_setting, aliens):
+    """Опускает весь флот и меняет направление флота."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_setting.fleet_drop_speed
+    ai_setting.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    """
+    Проверяет, достиг ли флот края экрана,
+    после чего обновляет позиции всех пришельцев во флоте.
+    """
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
 
