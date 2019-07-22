c763f85bb1121bcde5e229ecf35303122f9cc1eb
Unknown <make.me.loco@gmail.com>
2019-07-22 23:56:38 +0300

    p.278 a

--- Ch_13/alien_invasion/game_functions.py
+++ Ch_13/alien_invasion/game_functions.py
@@ -146,6 +146,16 @@ def change_fleet_direction(ai_setting, aliens):
     ai_setting.fleet_direction *= -1
 
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Проверяет, добрались ли пришельцы до нижнего края экрана."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Происходит то же, что при столкновении с кораблем.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """
     Проверяет, достиг ли флот края экрана,
@@ -156,6 +166,8 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Проверка коллизий "пришелец-корабль".
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    # Проверка пришельцев, добравшихся до нижнего края экрана.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 
 def update_screen(ai_settings, screen, ship, aliens, bullets, stars):
