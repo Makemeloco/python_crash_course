a8a41def6d622b94cd7fd9da7b0c68d1305a4f6e
Unknown <make.me.loco@gmail.com>
2019-07-25 22:09:19 +0300

    The play button starts the game.

--- Ch_14/alien_invasion/game_functions.py
+++ Ch_14/alien_invasion/game_functions.py
@@ -120,7 +120,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     # Отслеживание событий клавиатуры и мыши.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -129,6 +129,15 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Запускает новую игру при нажатии кнопки Play."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def check_fleet_edges(ai_settings, aliens):
