2a0c4671614681a2c1786559eaf59ae2bec72f22
Unknown <make.me.loco@gmail.com>
2019-08-03 14:11:36 +0300

    Change the type of labels and the thickness of the graph.
    p.313

--- Ch_15/mpl_squares.py
+++ Ch_15/mpl_squares.py
@@ -4,9 +4,9 @@ squares = [1, 4, 9, 16, 25]
 plt.plot(squares, linewidth=5)
 
 # Назначение заголовка диаграммы и меток осей.
-plt.title("Square Numbers", fontsize=14)
-plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+plt.title("Квадрат числа", fontsize=14)
+plt.xlabel("Значение числа", fontsize=14)
+plt.ylabel("Квадрат от числа", fontsize=14)
 
 # Назначение размера шрифта делений на осях.
 plt.tick_params(axis='both', labelsize=14)
