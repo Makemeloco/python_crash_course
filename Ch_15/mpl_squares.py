506b9e99b8bd2142b667d77324e4d88e7d807115
Unknown <make.me.loco@gmail.com>
2019-08-03 22:07:43 +0300

    Chart Adjustment.
    p.314

--- Ch_15/mpl_squares.py
+++ Ch_15/mpl_squares.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # Назначение заголовка диаграммы и меток осей.
-plt.title("Квадрат числа", fontsize=14)
+plt.title("Квадрат числа", fontsize=24)
 plt.xlabel("Значение числа", fontsize=14)
 plt.ylabel("Квадрат от числа", fontsize=14)
 
