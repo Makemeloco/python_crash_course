44d8789c9c216908c2b42e78871ebcd7c834ad2a
Unknown <make.me.loco@gmail.com>
2019-08-04 20:59:24 +0300

    Auto save charts.
    p.319

--- Ch_15/scatter_squares.py
+++ Ch_15/scatter_squares.py
@@ -17,4 +17,5 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 # Назначение диапазона для каждой оси.
 plt.axis([0, 1100, 0, 1100000])
 
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
