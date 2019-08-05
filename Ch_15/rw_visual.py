a9699236d7b0f9280cfc7872f735180dd64d6b58
Unknown <make.me.loco@gmail.com>
2019-08-05 22:01:09 +0300

    Generation of several random walks.
    p.322

--- Ch_15/rw_visual.py
+++ Ch_15/rw_visual.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Построение случайного блуждания и нанесение точек на диаграмму.
+# Новые блуждания строятся до тех пор, пока программа остается активной.
+while True:
+    # Построение случайного блуждания и нанесение точек на диаграмму.
 
-rw = RandomWalk()
+    rw = RandomWalk()
 
-rw.fill_walk()
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
