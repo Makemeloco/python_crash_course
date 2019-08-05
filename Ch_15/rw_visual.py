a45c70c507d3e8e6cffb4df717e9fe61c88652e8
Unknown <make.me.loco@gmail.com>
2019-08-05 23:52:04 +0300

    Dark background style.
    p.326

--- Ch_15/rw_visual.py
+++ Ch_15/rw_visual.py
@@ -6,16 +6,20 @@ from random_walk import RandomWalk
 while True:
     # Построение случайного блуждания
     # и нанесение точек на диаграмму.
-
     rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Темная цветовая схема.
+    plt.style.use('dark_background')
 
     # Назначение размера области просмотра.
     plt.figure(figsize=(10, 6))
 
+    # Генерация точек случайного блуждания.
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
                 cmap=plt.cm.viridis, edgecolor='none', s=1)
+
     # Выделение первой и последней точек.
     plt.scatter(0, 0, c='blue', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='yellow',
@@ -27,6 +31,8 @@ while True:
 
     plt.show()
 
+    # Предложение сгенерировать и отобразить
+    # ещё один экземпляр диаграммы (предидущий затирается).
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
         break
