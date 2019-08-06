1191b26081f33e0fd6d1d872b288d5b79d7d975b
Kate <make.me.loco@gmail.com>
2019-08-06 17:03:10 +0300

    Automatic tags.
    p.335

--- Ch_15/different_dice.py
+++ Ch_15/different_dice.py
@@ -22,8 +22,14 @@ for value in range(2, max_result + 1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling a D6 and a D10 50,000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
-                 '12', '13', '14', '15', '16']
+
+labels = []
+for value in range(2, max_result + 1):
+    label = str(value)
+    labels.append(label)
+
+hist.x_labels = labels
+
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
