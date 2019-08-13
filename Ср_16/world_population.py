e50e65b7f28e0f289711ee0eb5286ebd75d3a115
Unknown <make.me.loco@gmail.com>
2019-08-13 23:54:38 +0300

    Building a complete population map.
    p.356

--- Ср_16/world_population.py
+++ Ср_16/world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 # Список заполняется данными.
@@ -6,13 +7,20 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Вывод населения каждой страны за 2010 год.
+# Построение словаря с данными численности населения.
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
+            cc_populations[code] = population
         else:
             print('ERROR - ' + country_name)
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
