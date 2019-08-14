4c24f5091ab08d8a2ed47f036aa425a344f74d51
Kate <make.me.loco@gmail.com>
2019-08-14 17:49:16 +0300

    Summary of the most popular repositories.
    p.368

--- Ch_17/python_repos.py
+++ Ch_17/python_repos.py
@@ -11,7 +11,14 @@ response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации о репозиториях.
-
-
-# Обработка результатов.
-print(response_dict.keys())
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+print("\nSelected information about first repository:")
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
