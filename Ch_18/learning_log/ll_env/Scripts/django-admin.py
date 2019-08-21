6f69ffbf1f28083d3d8766df7a754b78e66c9f4f
Kate <make.me.loco@gmail.com>
2019-08-21 18:49:18 +0300

    Created a project Django on Windows.
    I had to roll back everything and start again. Not from home.
    p.383

---
+++ Ch_18/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\downloads\python_crash_course\ch_18\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
