529416c245dc300b4a40123b9748f0cd8f7e0d97
Kate <make.me.loco@gmail.com>
2019-09-03 18:58:04 +0300

    Activation of models.
    p.388

--- Ch_18/learning_log/learning_logs/models.py
+++ Ch_18/learning_log/learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):
     """Тема, которую изучает пользолватель."""
-    text = models.CharField(max_lenght=200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
