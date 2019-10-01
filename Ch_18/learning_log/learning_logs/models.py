74ccfed0fdd91dc8c3a274fa3c58189a109b09b7
Unknown <make.me.loco@gmail.com>
2019-10-01 21:53:24 +0300

    Register Entry on the administrative site.
    p.392

--- Ch_18/learning_log/learning_logs/models.py
+++ Ch_18/learning_log/learning_logs/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Информация, изученная пользователем по теме."""
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE,)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -24,3 +24,4 @@ class Entry(models.Model):
     def __str__(self):
         """Возвращает строковое представление модели."""
         return self.text[:50] + "..."
+
