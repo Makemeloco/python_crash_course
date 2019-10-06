9fd5a07c6d2ddd7c6ba033cd031e12e713ba789b
Unknown <make.me.loco@gmail.com>
2019-10-06 22:04:00 +0300

    The topics template.
    p.403

--- Ch_18/learning_log/learning_logs/urls.py
+++ Ch_18/learning_log/learning_logs/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     # Home page.
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
