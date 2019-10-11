3fa58e3499d8952fcb277081918cef797709c715
Unknown <make.me.loco@gmail.com>
2019-10-12 01:10:16 +0300

    URL for new_entry. View function new_entry ().
    p.415

--- Ch_18/learning_log/learning_logs/urls.py
+++ Ch_18/learning_log/learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница добавления новой записи.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
