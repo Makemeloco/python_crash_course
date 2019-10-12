9d3e5fd9503a1db75010cc698bb914d37c4ec94d
Unknown <make.me.loco@gmail.com>
2019-10-13 01:33:20 +0300

    Edit_entry template.
    p.418

--- Ch_18/learning_log/learning_logs/views.py
+++ Ch_18/learning_log/learning_logs/views.py
@@ -82,5 +82,5 @@ def edit_entry(request, entry_id):
                                                 args=[topic.id]))
 
 
-context = {'entry': entry, 'topic': topic, 'form': form}
-return render(request, 'learning_logs/edit_entry.html', context)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
