a0ed7f0da52fc8962aaa8d9058663d86da1f4581
Unknown <make.me.loco@gmail.com>
2019-11-10 23:51:54 +0300

    Link to the registration page.
    P.427

--- Ch_18/learning_log/users/views.py
+++ Ch_18/learning_log/users/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 
-# Create your views here.
 def register(request):
     """Регистрирует нового пользователя."""
     if request.method != 'POST':
